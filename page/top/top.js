f82e93f8486607a8d91fc7d23c8503a8595a8a46
Vier Espada <pohaltpk@gmail.com>
2026-06-01 11:23:41 +0900

    Update top.js

--- page/top/top.js
+++ page/top/top.js
@@ -1,8 +1,21 @@
 fetch("./top.json")
-.then(response => response.json())
-.then(data => {
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("guildName").textContent =
+            data.guildName;
+    });
 
-    document.getElementById("guildName").textContent =
-        data.guildName;
+const themeToggle =
+    document.getElementById("themeToggle");
+
+themeToggle.addEventListener("click", () => {
+
+    document.body.classList.toggle("light");
+
+    if (document.body.classList.contains("light")) {
+        themeToggle.textContent = "☀";
+    } else {
+        themeToggle.textContent = "🌙";
+    }
 
 });
